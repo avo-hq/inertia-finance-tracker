bf799611dcfbb76cb5e53b20da2348bfd2a4f8d0
Exequiel Rozas <exequiel98@gmail.com>
2025-01-30 21:33:59 -0400

    Redirect to 'dashboard_url' after a successful sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,7 +11,7 @@ class SessionsController < ApplicationController
   def create
     if user = User.authenticate_by(params.permit(:email_address, :password))
       start_new_session_for user
-      redirect_to after_authentication_url, notice: "Successfully signed in."
+      redirect_to dashboard_url, notice: "Successfully signed in."
     else
       redirect_to new_session_path, alert: "Invalid email address or password."
     end
