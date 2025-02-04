93d591345f422aefcf9d2e6f5736be7f9b425cb2
Exequiel Rozas <exequiel98@gmail.com>
2025-02-04 00:57:44 -0400

    Transaction index and ability to delete them

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -29,17 +29,41 @@ class Transaction < ApplicationRecord
   belongs_to :user
   belongs_to :category
 
-  validates :amount_cents, presence: true
+  validates :amount, presence: true
   validates :transaction_type, presence: true, inclusion: { in: TRANSACTION_TYPES }
   validates :date, presence: true
 
-  monetize :amount_cents, as: :amount
+  monetize :amount_cents
 
   scope :descending_by_date, -> { order(date: :desc) }
   scope :income, -> { where(transaction_type: 'income') }
   scope :expense, -> { where(transaction_type: 'expense') }
 
+  before_save :ensure_amount_sign
+  after_save :update_user_balance
+
+  def self.daily_amounts(user)
+    daily_totals = expense.group_by_day(:date).sum(:amount_cents)
+    
+    daily_totals.map do |date, amount|
+      {
+        x: date.to_time.to_i * 1000, # date is already a Date object
+        y: amount.abs / 100.0
+      }
+    end.sort_by { |point| point[:x] }
+  end
+  
   def category_name
     category.name
+  end
+
+  private
+
+  def ensure_amount_sign
+    self.amount = -self.amount if transaction_type == 'expense'
+  end
+
+  def update_user_balance
+    user.update!(balance: user.balance + amount)
   end
 end
